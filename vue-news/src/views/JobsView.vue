7e71d8aff8123abe0169a0f8d7a35c622906cc56
Hanna Park <swucs7@gmail.com>
2020-08-15 20:50:38 +0900

    api 적용

--- vue-news/src/views/JobsView.vue
+++ vue-news/src/views/JobsView.vue
@@ -1,12 +1,26 @@
 <template>
 	<div>
-		Jobs
+		<div v-for="job in jobs" v-bind:key="job.id">{{ job.title }}</div>
 	</div>
 </template>
 
 <script>
+import { fetchJobsList } from '../api/index.js';
 export default {
-
+	data() {
+		return {
+			jobs : []
+		}
+	},
+	created() {
+		fetchJobsList()
+			.then(response => {
+				this.jobs = response.data;
+			})
+			.catch(error => {
+				console.log(error);
+			});
+	}
 }
 </script>
 
--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -1,12 +1,28 @@
 <template>
 	<div>
-		News
+		<div v-for="user in users" v-bind:key="user.id">{{ user.title }}</div>
 	</div>
 </template>
 
 <script>
-export default {
+import { fetchNewsList } from '../api/index.js';
 
+export default {
+	data() {
+		return {
+			users : []
+		}
+	},
+	created() {
+		var vm = this;
+
+		fetchNewsList()
+			.then(response => {
+				console.log(response);
+				vm.users = response.data;
+			})
+			.catch(error => console.log(error));
+	}
 }
 </script>
 
